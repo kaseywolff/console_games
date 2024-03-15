fcb5aed2b6127becdd3f117d19785f1af28ed49a
kaseywolff <kaseywolff5@gmail.com>
2024-03-15 16:29:41 -0500

    functions to check valid move and make a move on the board

--- Connect4/connect4.py
+++ Connect4/connect4.py
@@ -9,9 +9,9 @@ NEEDED_TO_WIN = 4 # how many you need to get in a row to win the game
 
 # print function for printing column header/ footer
 def print_header_footer():
-  print('  ', end='')
+  print('   ', end='')
   for i in range(BOARD_SIZE):
-    print(i, end=' ')
+    print(i, end='  ')
   print()
   
 
@@ -23,14 +23,14 @@ def print_board(board):
 
   # print the board itself
   for i in range(BOARD_SIZE):
-    print(' ', end=' ')
+    print('  ', end=' ')
     for j in range(BOARD_SIZE):
       if board[i][j] == PLAYER1_SYMBOL:
-        print(PLAYER1_SYMBOL, end=' ')
+        print(PLAYER1_SYMBOL, end='  ')
       elif board[i][j] == PLAYER2_SYMBOL:
-        print(PLAYER2_SYMBOL, end=' ')
+        print(PLAYER2_SYMBOL, end='  ')
       else:
-        print('.', end=' ')
+        print('.', end='  ')
     print()
   
   # print column number footer
@@ -48,6 +48,31 @@ def player_move_input(player_symbol):
     except ValueError:
       pass
     print('Invalid input. Please enter a number between 0 and', BOARD_SIZE-1)
+
+
+# function to check if the move is valid
+def is_valid_move(board, column):
+  # loop through row #s, starting from the bottom. in this case, the bottom row is the board size - 1
+  for i in range(BOARD_SIZE - 1, -1, -1):
+    # if the row in the specified column is empty ('.'), return the row #
+    if board[i][column] == '.':
+      return i
+    return -1
+  
+
+# function to execute a move on the board
+def make_move(board, player_symbol, column):
+  row = is_valid_move(board, column)
+
+  if row != -1:
+    board[row][column] = player_symbol
+  else:
+    print(f'This column is full. Player {player_symbol}, please try again and enter a different column.')
+    make_move(board, player_symbol, player_move_input(player_symbol))
+
+
+# function to check if the player has won
+
 
 
 def play_game():
